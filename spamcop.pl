f94a070516d9f67b8b37b863de78c2d78785a5ae
Mark eM <Monter@users.noreply.github.com>
2014-12-29 15:12:05 +0100

    Update spamcop.pl

--- spamcop.pl
+++ spamcop.pl
@@ -1,14 +1,13 @@
 #!/usr/bin/perl
 #
-# SpamCop.net - Automatic approval of spam reports v2.6 (2014-10-15)
+# SpamCop.net - Automatic approval of spam reports v2.7 (2014-12-29)
 # Written by Monter - http://monter.techlog.pl/files/download/_Projects/Linux/spamcop/
+#                     https://github.com/Monter/spamcop.pl
 #
-# v2.6 - added a new section that detects message "No body text provided, check format of submission" which makes it impossible to send a report, because sub
-mitted spam does not contain BODY section.
-# v2.5 - added a new section that detects message "Sorry, this email is too old to file a spam report" which makes it impossible to send a report, because Yo
-u must report spam within 2 days of receipt.
-# v2.4 - added a new section that detects message "Reports regarding this spam have already been sent" which makes it impossible to send a report, because th
-e report of the spam has been sent (spam is old).
+# v2.7 - added debugging option (the write report on disk) and other minor improvements
+# v2.6 - added a new section that detects message "No body text provided, check format of submission" which makes it impossible to send a report, because submitted spam does not contain BODY section.
+# v2.5 - added a new section that detects message "Sorry, this email is too old to file a spam report" which makes it impossible to send a report, because You must report spam within 2 days of receipt.
+# v2.4 - added a new section that detects message "Reports regarding this spam have already been sent" which makes it impossible to send a report, because the report of the spam has been sent (spam is old).
 #      - added information about the address at which you can view the report
 #      - added a function to display the date and time
 #      - make script output as unbuffered
@@ -18,13 +17,14 @@ e report of the spam has been sent (spam is old).
 # v2   - added support for advertising appearing in random moments ("Please wait - subscribe to remove this delay...")
 # v1   - initial basic version
 #
+# Bug reports and suggestions for improvements send an email monter[at]techlog.pl
 
 use strict;
 use warnings;
 use POSIX qw/strftime/;
 use WWW::Mechanize;
 
-$| = 1;
+$| = 1; # unbuffered output
 print "\n@@ ".strftime('%F %T',localtime)."\n";
 
 my $spamcop_url = 'http://www.spamcop.net';
@@ -64,10 +64,14 @@ if (defined $foundLink) {
         print "## Sorry, this email is too old to file a spam report. You must report spam within 2 days of receipt. See: ".$mech->uri()."\n";
       } elsif ($mech->content =~ /No.body.text.provided/) {
         print "## No body text provided, check format of submission. Spam must have body text. See: ".$mech->uri()."\n";
-      } else {
+      } elsif ($mech->content =~ /Send.Spam.Report/) {
         my $form = $mech->form_name( 'sendreport' );
         print "-> Send report: ".$mech->value('reports')." - see this report at: ".$mech->uri()."\n";
         $mech->click_button( 'value' => 'Send Spam Report(s) Now' );
+      } else {
+        print "!! An unknown error occurred. Please try after some time. If the problem persists, report it to the spamcop.pl author.\n";
+        # debug
+        #$mech->save_content("error_report");
       }
     } else {
       print ":: No more report(s) found. Done!\n";
